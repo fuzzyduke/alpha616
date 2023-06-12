2bf8d40523cce726fb787e4dfb812025d71435de
fuzzyduke <gracias.edsel@gmail.com>
2023-06-12 21:37:50 +0300

    csv file change

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 
 object = {"key": "value"}
 
-output_file = open("output/data.json", "w")
+output_file = open("output/entityTB.csv", "w")
 output_file.truncate(0)
 output_file.write(json.dumps(object))
 output_file.close()
